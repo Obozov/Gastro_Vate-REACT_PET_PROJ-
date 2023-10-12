0eb78f2e7d35b9bfb66ae96ba905a14e03de77f3
DenysO <3id3i19@gmail.com>
2023-10-13 00:17:55 +0200

    Creating header logic+imports files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './Components/Footer/footer';
+import Header from './Components/Header/Header';
+import Map from './Components/Main/map/map';
+import List from './Components/Main/list/list';
+import About from './Components/Main/about/about';
+import Rev_Sup from './Components/Main/rev/reviews';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
 	return (
-		<div className="App">
-			to
-		</div>
+		<BrowserRouter>
+			<div className="App">
+				<Header />
+
+				<div className='app-wrapper-content'>
+					<Routes>
+						<Route path='/map' element={<Map />} />
+						<Route path='/list' element={<List />} />
+						<Route path='/about' element={<About />} />
+						<Route path='/rev_sup' element={<Rev_Sup />} />
+					</Routes>
+				</div>
+				<Footer />
+			</div>
+		</BrowserRouter>
 	);
+
 }
 
 export default App;
